d0f8723431e84522f18ab3c25dfba3a033d6e376
yjiang <yjiang@roblox.com>
2021-05-06 11:53:05 -0700

    fix: add comments 1153

--- packages/usage/index.js
+++ packages/usage/index.js
@@ -1,3 +1,5 @@
-const alpha = require("@ying414/alpha");
-const beta = require("@ying414/beta");
-console.log(alpha + " " + beta);
+// usage package
+
+const alpha = require('@ying414/alpha');
+const beta = require('@ying414/beta');
+console.log(alpha + ' ' + beta);
